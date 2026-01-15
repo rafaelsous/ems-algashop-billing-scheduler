ad6c5ce0e81dc008fbe18a72fd4e1b12f6167b16
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-01-15 17:08:05 -0300

    EMS-N-03-09.09 - Adjusting the application to be a short-lived microservice.

--- src/main/java/com/rafaelsousa/algashop/billingscheduler/BillingSchedulerApplication.java
+++ src/main/java/com/rafaelsousa/algashop/billingscheduler/BillingSchedulerApplication.java
@@ -4,9 +4,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class BillingSchedulerApplication {
--- src/main/java/com/rafaelsousa/algashop/billingscheduler/infrastructure/CancelExpiredInvoiceRunner.java
+++ src/main/java/com/rafaelsousa/algashop/billingscheduler/infrastructure/CancelExpiredInvoiceRunner.java
@@ -3,17 +3,18 @@ package com.rafaelsousa.algashop.billingscheduler.infrastructure;
 import com.rafaelsousa.algashop.billingscheduler.application.CancelExpiredInvoicesApplicationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class CancelExpiredInvoiceScheduler {
+public class CancelExpiredInvoiceRunner implements ApplicationRunner {
     private final CancelExpiredInvoicesApplicationService cancelExpiredInvoicesApplicationService;
 
-    @Scheduled(fixedRate = 1000 * 5) // 5 seconds
-    public void runTask() {
+    @Override
+    public void run(ApplicationArguments args) {
         log.info("Task started - Cancelling expired invoices.");
 
         cancelExpiredInvoicesApplicationService.cancelExpiredInvoices();
